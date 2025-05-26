ce9d2d5cd27d33e75e7d46bbefbb81e3383f6f91
Hani <hanijinow@gmail.com>
2025-05-26 19:46:17 +0200

    create test for counting columns

---
+++ columnsTests.py
@@ -0,0 +1,18 @@
+import unittest
+import pandas as pd
+
+class TestCSVColumns(unittest.TestCase):
+    def setUp(self):
+        self.df = pd.read('profiles1.csv')
+
+    def test_has_12_columns(self):
+        self.assertEqual(self.df.shape[1], 12, "Expected 12 columns")
+
+    def test_not_less_than_12_columns(self):
+        self.assertFalse(self.df.shape[1] < 12, "Too few columns")
+
+    def test_not_more_than_12_columns(self):
+        self.assertFalse(self.df.shape[1] > 12, "Too many columns")
+
+if __name__ == '__main__':
+    unittest.main()
